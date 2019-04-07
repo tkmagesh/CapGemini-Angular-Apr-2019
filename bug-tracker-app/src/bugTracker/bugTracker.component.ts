e77f2c13c0a7afe80f8d21e9ca736a9e3e393214
Magesh <tkmagesh77@gmail.com>
2019-04-07 11:10:01 +0530

    removing closed bugs and displaying stats

--- bug-tracker-app/src/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/bugTracker/bugTracker.component.ts
@@ -24,4 +24,14 @@ export class BugTrackerComponent{
 	onBugNameClick(bugToToggle : Bug){
 		bugToToggle.isClosed = !bugToToggle.isClosed;
 	}
+
+	onRemoveClosedClick(){
+		this.bugs = this.bugs.filter(bug => !bug.isClosed);
+	}
+
+	getClosedCount(){
+		return this.bugs.reduce((result, bug) => bug.isClosed ? ++result : result, 0);
+	}
+
+
 }
